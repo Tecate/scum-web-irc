0ac1c62a7c7a8836fefc2aa0e6c5786aab4cffbb
James <kawnsull@gmail.com>
2018-06-04 21:56:01 -0400

    handles command inputs

--- websocket.js
+++ websocket.js
@@ -1,31 +1,38 @@
 var irc = require('irc');
 
-var channel = '#general';
-var nick = 'nodebot';
+var channel = '#testing';
 var connected = false;
 
 exports = module.exports = function(io){
 	io.on('connection', function(socket){
 		console.log('a user connected');
 
-		var client = new irc.Client('scum.systems', nick, {
+		var client = new irc.Client('scum.systems', 'nodebot', {
 			port: '6697',
 			secure: true,
 			channels: [channel],
 			autoConnect: false
 		});
 
-		client.connect(function() {
-			client.addListener('error', function(message) {
-			    console.log('error: ', message);
-			});
+		client.addListener('error', function(message) {
+		    console.log('error: ', message);
+		});
 
-			client.addListener('join', function(channelName, nickName, message) {
-				if (nickName === client.nick) { // only run when we connect, join fires on all joins
-					client.say(channel, "I'm a bot!");
-					connected = true;
-				}
-			});
+		client.addListener('whois', function(message) {
+		    console.log(message);
+		    io.emit("whois", message);
+		});
+
+		client.connect(function() {
+
+			// client.addListener('raw', function(message) {
+			//     console.log(message);
+			// });
+
+			client.join(channel, function(){
+				client.say(channel, "I'm a bot!");
+				connected = true;
+			})
 
 			client.addListener('message', function(nickName, to, text, message) {
 				console.log(nickName + ": " + text);
@@ -33,13 +40,19 @@ exports = module.exports = function(io){
 			});
 
 			socket.on('nick', function(data){
-				nick = data;
-				client.say("/nick" + nick);
+				client.send('nick', data);
 			});
 
 			socket.on('message', function(data){
 				console.log(client.nick + ": " + data);
-				client.say(channel, data);
+				if (data.substring(0,1) === '/') {
+					command = data.substring(1);
+					command = command.split(" ");
+					console.log(command);
+					client.send(...command);
+				} else {
+					client.say(channel, data);
+				}
 			});
 
 			socket.on('disconnect', function(){
